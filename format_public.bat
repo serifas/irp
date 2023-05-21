b3e16a1ccc7df7431d2516b1f92b02b83d860158
Omie <preachandteach@outlook.com>
2023-05-21 16:07:36 -0700

    latest with server push

---
+++ format_public.bat
@@ -0,0 +1,28 @@
+echo off
+title Custom Text File
+cls
+echo [
+    {
+        "Author": "Lynie",
+        "Name": "Infinite Roleplay",
+        "Description": "Tool for creating and sharing roleplay profiles, stat and combat systems, events and more.",
+        "InternalName": "InfiniteRoleplay",
+        "AssemblyVersion": "1.0.0.3",
+        "TestingAssemblyVersion": "1.0.0.3",
+        "RepoUrl": "https://github.com/serifas/irp",
+        "ApplicableVersion": "any",
+        "DalamudApiLevel": 8,
+        "IsHide": "False",
+        "IsTestingExclusive": "False",
+        "DownloadCount": 0,
+        "LastUpdate": 0,
+        "LoadPriority": 69420,
+        "LoadRequiredState": 2,
+        "LoadSync": true,
+        "DownloadLinkInstall": "https://github.com/serifas/irp/raw/main/latesttest.zip",
+        "DownloadLinkTesting": "https://github.com/serifas/irp/raw/main/latesttest.zip",
+        "DownloadLinkUpdate": "https://github.com/serifas/irp/raw/main/latesttest.zip",
+        "IconUrl": "https://raw.githubusercontent.com/serifas/irp/main/j1Qgzx9.png"
+    }
+] > C:\Users\Preac\Documents\infinite-rp\infinitetest_public.json"
+exit
